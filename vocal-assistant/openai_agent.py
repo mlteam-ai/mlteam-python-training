56eb7fd841b64571b02b8629a0d572e9a4e580b3
Meltem Seyhan <meltem.yesiltas@gmail.com>
2024-01-16 01:19:43 +0300

    Refactored OpenAIAgent class

--- vocal-assistant/openai_agent.py
+++ vocal-assistant/openai_agent.py
@@ -10,31 +10,30 @@ class OpenAIAgent:
     def __init__(self, model="gpt-3.5-turbo") -> None:
         self.model = model
         self.client = openai.OpenAI()
+
+    def __create_chat_completion(self, messages):
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=messages
+        )
+        return response.choices[0].message.content
     
     def get_response(self, command):
-        response = self.client.chat.completions.create(
-            model=self.model,
-            messages=[
-                {"role": "system", "content": "You are a vocal assistant. You have to answer in a simple, efficient and concise way. Your answer should not take more than 30 seconds to say out loud"},
-                {"role": "user", "content": command},
-            ]
-        )
-        assistant_reply = response.choices[0].message.content
-        return assistant_reply
+        messages=[
+            {"role": "system", "content": "You are a vocal assistant. You have to answer in a simple, efficient and concise way. Your answer should not take more than 30 seconds to say out loud"},
+            {"role": "user", "content": command},
+        ]
+        return self.__create_chat_completion(messages)
     
     def get_command_label(self, command):
-        response = self.client.chat.completions.create(
-            model=self.model,
-            messages=[
-                {"role": "system", "content": "You are a vocal assistant."},
-                {"role": "system", "content": "Your role is to classify the user's command and return only the corresponding label."},
-                {"role": "system", "content": "The labels are: to-do list, normal question"},
-                {"role": "system", "content": "If you recognize the user's command as a to-do list request (for example), then return 'to-do list'."},
-                {"role": "user", "content": command},
-            ]
-        )
-        label = response.choices[0].message.content
-        return label
+        messages=[
+            {"role": "system", "content": "You are a vocal assistant."},
+            {"role": "system", "content": "Your role is to classify the user's command and return only the corresponding label."},
+            {"role": "system", "content": "The labels are: to-do list, normal question"},
+            {"role": "system", "content": "If you recognize the user's command as a to-do list request (for example), then return 'to-do list'."},
+            {"role": "user", "content": command},
+        ]
+        return self.__create_chat_completion(messages)
     
     def get_audio_from_text(self, text):
         response = self.client.audio.speech.create(
